f0821c7f51218c7b857a5e20c4778e34f2e5e9e5
davidcliddell <david@liddell.com>
2021-04-15 12:55:15 +0100

    added write_buf and read_buf

--- src/FRAM_I2C.cpp
+++ src/FRAM_I2C.cpp
@@ -7,7 +7,7 @@ bool FRAM_I2C::begin(uint8_t addr) {
   Wire.begin();
   Wire.beginTransmission(addr);
   if (Wire.endTransmission()) {
-    pfln("end failed");
+    pfln("FRAM_I2C end failed");
     enabled_ = 0;
     return(0);
   }
@@ -15,35 +15,50 @@ bool FRAM_I2C::begin(uint8_t addr) {
   return(1);
 }
 
-void FRAM_I2C::write8(uint16_t addr, uint8_t value) {
+void FRAM_I2C::write8(uint16_t offset, uint8_t value) {
   Wire.beginTransmission(i2c_addr_);
-  Wire.write(addr >> 8);
-  Wire.write(addr & 0xFF);
+  Wire.write(offset >> 8);
+  Wire.write(offset & 0xFF);
   Wire.write(value);
   Wire.endTransmission();
 }
 
-void FRAM_I2C::write32(uint16_t addr, uint32_t value) {
+void FRAM_I2C::write32(uint16_t offset, uint32_t value) {
   size_t i;
   uint8_t *ptr = (uint8_t *)&value;
   for (i = 0;i < sizeof(uint32_t);i++)
-    write8(addr+i, ptr[i]);
+    write8(offset+i, ptr[i]);
 }
 
-uint8_t FRAM_I2C::read8(uint16_t addr) {
+void FRAM_I2C::write_buf(uint8_t *buf, uint16_t offset, uint16_t size) {
+  size_t i;
+
+  for (i = 0;i < size;i++) {
+    write8(offset+i, buf[i]);
+  }
+}
+
+uint8_t FRAM_I2C::read8(uint16_t offset) {
   Wire.beginTransmission(i2c_addr_);
-  Wire.write(addr >> 8);
-  Wire.write(addr & 0xFF);
+  Wire.write(offset >> 8);
+  Wire.write(offset & 0xFF);
   Wire.endTransmission();
   Wire.requestFrom(i2c_addr_, (uint8_t)1);
   return Wire.read();
 }
 
- uint32_t FRAM_I2C::read32(uint16_t addr) {
+ uint32_t FRAM_I2C::read32(uint16_t offset) {
   size_t i;
   uint32_t res;
   uint8_t *ptr = (uint8_t *)&res;
   for (i = 0;i < sizeof(uint32_t);i++)
-    ptr[i] = read8(addr+i);
+    ptr[i] = read8(offset+i);
   return(res);
+}
+
+void FRAM_I2C::read_buf(uint8_t *buf, uint16_t offset, uint16_t size) {
+  size_t i;
+  for (i = 0;i < size;i++) {
+    buf[i] = read8(offset+i);
+  }
 }
--- src/FRAM_I2C.h
+++ src/FRAM_I2C.h
@@ -4,10 +4,12 @@
 class FRAM_I2C {
 public:
   bool begin(uint8_t addr = 0x50);
-  void write8(uint16_t i2c_addr, uint8_t value);
-  void write32(uint16_t i2c_addr, uint32_t value);
-  uint8_t read8(uint16_t i2c_addr);
-  uint32_t read32(uint16_t i2c_addr);
+  void write8(uint16_t offset, uint8_t value);
+  void write32(uint16_t offset, uint32_t value);
+  void write_buf(uint8_t *buf, uint16_t offset, uint16_t size);
+  uint8_t read8(uint16_t offset);
+  uint32_t read32(uint16_t offset);
+  void read_buf(uint8_t *buf, uint16_t offset, uint16_t size);
 private:
   uint8_t i2c_addr_;
   bool enabled_ = 0;
